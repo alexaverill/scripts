e32cc44689e09c950d91e686c153b5fb1778b2d9
aaverill <alex.averill2013@gmail.com>
2013-11-13 17:34:42 -0600

    Comments?

--- emails.sh
+++ emails.sh
@@ -2,10 +2,12 @@
 echo Please enter the new email name  and the email to direct to: 
 read NAME ToGo
 echo You have entered $NAME and it will go to $ToGo
+
 #mkdir /home/$NAME
-useradd -m $NAME
-echo "$NAME:$ToGo" >> /etc/aliases
+useradd -m $NAME		#adds user and home dir
+echo "$NAME:$ToGo" >> /etc/aliases	
 newaliases
 service postfix restart
+passwd -l $NAME 		#locks user account
 echo Emails Added, please test.
 
