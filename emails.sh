21ee24295cc6ca2ae101508a2b827ebb45ad385d
aaverill <alex.averill2013@gmail.com>
2013-11-13 18:06:58 -0600

    Adding user validation

--- emails.sh
+++ emails.sh
@@ -1,9 +1,18 @@
 #o Script to add email redirection.
+correct=true
+while :
+do
 echo Please enter the new email name  and the email to direct to: 
 read NAME ToGo
-echo You have entered $NAME and it will go to $ToGo
+echo You have entered $NAME and it will go to $ToGo is this correct? y 0r n
+read r
+if test "$r" = "y"
+	then
+	 	set correct=false
+		break
+	fi
+done
 
-#mkdir /home/$NAME
 useradd -m $NAME		#adds user and home dir
 echo "$NAME:$ToGo" >> /etc/aliases	
 newaliases
