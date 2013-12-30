fb4b3d7932e7d9a6d49dbd71770651f710aa35f7
alexaverill <alex.averill201@gmail.com>
2013-12-30 14:15:39 -0600

    ESUS

--- status.sh
+++ status.sh
@@ -1,16 +1,19 @@
 #! /bin/bash
 sites[0]="scioly.org"
 sites[1]="192.168.2.5" #file_server
+sites[2]="192.168.2.2"
+check_status(){
+      ping -c 1 $1 >> /dev/null
+      st=$?
+      if [ $st -eq 1 ] 
+      then
+ 		echo $1 "Has a problem"
+		mail -s "$1 has a problem" "alex@scioly.org"   
+   fi	
+}
 x=0
-while [ $x -le 1 ]
+while [ $x -le 2 ]
 do
-   ping -c 1  ${sites[$x]} > /dev/null
-   echo $?
-   if (( $? == 0 ))
-   then
-	echo ${sites[$x]} " was pinged"
-   else
-	echo ${sites[$x]} " HAS NO PING"
-   fi 
+   check_status ${sites[$x]}
    let "x+=1"
 done
