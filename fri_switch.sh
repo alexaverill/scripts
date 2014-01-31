623a4bcb236f7f2cc0ed63db382180a100a085e7
alexaverill <alex.averill201@gmail.com>
2014-01-30 22:43:37 -0600

    Adding to switch script

--- fri_switch.sh
+++ fri_switch.sh
@@ -1,15 +1,19 @@
 #! /bin/bash
 #this script rotates sciolys homepage at certain times
-# 1 switch to friday screen 2 back to normal
-loc=/var/www/sites/scioly.org/public/
-if [[ $1 -eq  2 ]];
+# 1 switch to mid day 2 to friday screen 3 back to normal
+loc=/var/www/sites/scioly.org/public
+if [[ $1 -eq  3 ]];	
 then 
 	sudo mv $loc/index.html $loc/fr_index.html
 	sudo mv $loc/bk_index.html $loc/index.html
 fi
-if [[ $1 -eq 1 ]];
+if [[ $1 -eq 2 ]];
 then 
-	sudo mv $loc/index.html $loc/bk_index.html
+	sudo mv $loc/index.html $loc/fr_pr_index.html
 	sudo mv $loc/fr_index.html $loc/index.html
 fi
-
+if [[ $1 -eq 1 ]];
+then
+        sudo mv $loc/index.html $loc/bk_index.html
+        sudo mv $loc/fr_pr_index.html $loc/index.html
+fi
